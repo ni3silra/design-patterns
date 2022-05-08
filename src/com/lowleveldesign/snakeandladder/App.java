423715891124790eab969a1f296c33e2dc2cccb0
NS098984 <nitin_silra@epam.onmicrosoft.com>
2022-05-08 21:25:31 +0200

    Added     - Snake and Ladder Fix

--- src/com/lowleveldesign/snakeandladder/App.java
+++ src/com/lowleveldesign/snakeandladder/App.java
@@ -16,37 +16,37 @@ public class App {
     private List<Jumper> snakes;
     private List<Jumper> ladders;
 
-    private App(){
+    private App() {
         addDiceAndBoard();
         addSnakesAndLadders();
         addPlayers();
     }
 
-    private void addDiceAndBoard(){
+    private void addDiceAndBoard() {
         boardSize = 100;
         dice = new Dice(1);
     }
 
-    private void addSnakesAndLadders(){
+    private void addSnakesAndLadders() {
         snakes = new ArrayList<>();
-        snakes.add(new Jumper(15,3));
-        snakes.add(new Jumper(26,12));
-        snakes.add(new Jumper(99,2));
+        snakes.add(new Jumper(15, 3));
+        snakes.add(new Jumper(26, 12));
+        snakes.add(new Jumper(99, 2));
 
         ladders = new ArrayList<>();
-        ladders.add(new Jumper(16,32));
-        ladders.add(new Jumper(9,12));
-        ladders.add(new Jumper(6,72));
-        ladders.add(new Jumper(7,51));
+        ladders.add(new Jumper(16, 32));
+        ladders.add(new Jumper(9, 12));
+        ladders.add(new Jumper(6, 72));
+        ladders.add(new Jumper(7, 51));
     }
 
-    private void addPlayers(){
-        Player akshay = new Player(1,"Akshay Kumar");
-        Player srk = new Player(2,"Shah Rukh Khan");
+    private void addPlayers() {
+        Player akshay = new Player(1, "Akshay Kumar");
+        Player srk = new Player(2, "Shah Rukh Khan");
 
         players = new HashMap<>();
         players.put(akshay.getName(), 0);
-        players.put(srk.getName(),0);
+        players.put(srk.getName(), 0);
 
         playQueue = new LinkedList<>();
         playQueue.offer(akshay);
@@ -55,7 +55,7 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        GameBoard board = new GameBoard(app.boardSize, app.dice,app.playQueue,app.snakes,app.ladders,app.players);
+        GameBoard board = new GameBoard(app.boardSize, app.dice, app.playQueue, app.snakes, app.ladders, app.players);
         board.startGame();
     }
 }
