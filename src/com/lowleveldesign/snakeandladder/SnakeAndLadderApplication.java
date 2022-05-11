0842ef7f5945edc0378d4569d9dded879916401e
ni3silra <ni3silra.sati@gmail.com>
2022-05-11 13:03:15 +0200

    New Player Added

--- src/com/lowleveldesign/snakeandladder/SnakeAndLadderApplication.java
+++ src/com/lowleveldesign/snakeandladder/SnakeAndLadderApplication.java
@@ -1,19 +1,17 @@
 package com.lowleveldesign.snakeandladder;
 
-import com.lowleveldesign.snakeandladder.businesslogic.GamePlay;
-import com.lowleveldesign.snakeandladder.businesslogic.GameSettings;
-import com.lowleveldesign.snakeandladder.businesslogic.PlayerSettings;
+import com.lowleveldesign.snakeandladder.businesslogic.*;
 import com.lowleveldesign.snakeandladder.entity.*;
 import com.lowleveldesign.snakeandladder.level.Level;
 
 import java.util.List;
 
 public class SnakeAndLadderApplication {
-
     public static void main(String[] args) {
         List<Player> playerList = List.of(
-                new Player("Akshay Kumar", 0),
-                new Player("Shah Rukh Khan", 0));
+                new Player("Ajay Devgan", 0),
+                new Player("Shah Rukh Khan", 0),
+                new Player("Akshay Kumar", 0));
 
         GameSettings.setLevel(Level.EASY);
         PlayerSettings.addPlayers(playerList);
